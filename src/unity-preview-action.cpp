e10e0701de230ffdda5b46e39b9ef8fdde5d09bd
Antti Kaijanmäki <antti.kaijanmaki@canonical.com>
2013-06-10 15:15:12 +0300

    Introduce PreviewAction

---
+++ src/unity-preview-action.cpp
@@ -0,0 +1,50 @@
+/* This file is part of unity-action-api
+ * Copyright 2013 Canonical Ltd.
+ *
+ * unity-action-api is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License version 3,
+ * as published by the Free Software Foundation.
+ *
+ * unity-action-api is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranties of
+ * MERCHANTABILITY, SATISFACTORY QUALITY, or FITNESS FOR A PARTICULAR
+ * PURPOSE.  See the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "unity-preview-action.h"
+
+using namespace unity::action;
+
+//! \private
+class UNITY_ACTION_PRIVATE_API unity::action::PreviewAction::Private {
+public:
+    QString commitLabel;
+};
+
+PreviewAction::PreviewAction(QObject *parent)
+    : Action(parent),
+      d(new Private())
+{
+}
+
+PreviewAction::~PreviewAction()
+{
+}
+
+QString
+PreviewAction::commitLabel() const
+{
+    return d->commitLabel;
+}
+
+void
+PreviewAction::setCommitLabel(const QString &value)
+{
+    if (d->commitLabel == value)
+        return;
+    d->commitLabel = value;
+    emit commitLabelChanged(value);
+}
